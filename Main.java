16f73524bc18a014706878a0119c3890514e8f45
azim-sht <azimstybaev31@gmail.com>
2024-12-24 16:46:07 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,16 @@
-package kg.attractor.java.task2;
+package kg.attractor.java.task1;
 
 public class Main {
 
     public static void main(String[] args) {
-        // исправьте код так, что бы на экран выводилось что-то вроде.
-        // Я Misty. Я прыгаю!
-        // Я Tibbles. Я сплю!
-        // Я Ginger. Я кушаю!
-        // для решения примените лямбда-выражения, 
-        
-        // каждый кот должен уметь выполнять что-то своё
-        var cats = new ArrayList<ActiveCat>();
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
-        cats.add(new ActiveCat(???));
-        // добавьте ещё два-три кота, с совершенно другими действиями
 
-        cats.forEach(ActiveCat::doAction);
+        var cats = Cat.makeCats(10);
+        Printer.print(cats);
+
+        // А сюда добавьте код, который будет сортировать коллекцию котов
+        // используйте лямбда-выражения и ссылки на методы
+        // cats.sort(?);
+        // Printer.print(cats);
     }
 
 }
